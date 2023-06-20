31441096d586fbeceace3b19729f7d48c9fca231
SebastianKaltenbachIBM <Sebastian.Kaltenbach1@ibm.com>
2023-06-20 22:10:22 +0200

    Add Camunda dependencies

--- src/main/java/com/ibm/ReportingMojo.java
+++ src/main/java/com/ibm/ReportingMojo.java
@@ -3,15 +3,16 @@ package com.ibm;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import com.ibm.model.ModelCollector;
 
-@Mojo(name = "report")
-public class ParserMojo extends AbstractMojo {
+@Mojo(threadSafe = true, name = "report", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class ReportingMojo extends AbstractMojo {
 
-    @Parameter(property = "sourceFolder", defaultValue = "src/main/resources")
+    @Parameter(required = true, defaultValue = "${project.basedir}/src/main/resources", property = "name.source")
     private String sourceFolder;
 
     @Parameter(property = "resultFile", defaultValue = "target/generated-source/dummy/bpmn_validation.result.yaml")
--- src/main/java/com/ibm/model/ModelCollector.java
+++ src/main/java/com/ibm/model/ModelCollector.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileFilter;
 import java.util.List;
 
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+
 public class ModelCollector {
 
     private String sourceFolder;
-    private List<String> modelPaths;
+    private List<BpmnModelInstance> modelInstances;
 
     private final FileFilter bpmnFilefilter = new FileFilter()
     {
@@ -23,12 +26,11 @@ public class ModelCollector {
         this.sourceFolder = sourceFolder;
     }
 
-
     public void collectModelsInSourceFolder() {
         File directory = new File(this.sourceFolder);   
-        File[] bpmnFiles = directory.listFiles(bpmnFilefilter);
 
-        for(File file : bpmnFiles) {
+        for(File file : directory.listFiles(bpmnFilefilter)) {
+            modelInstances.add(Bpmn.readModelFromFile(file));
             System.out.println(file.getName());
         }
     }
