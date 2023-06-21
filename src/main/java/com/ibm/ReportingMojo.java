bb1704f57e229f5c2ea702c6bb8c043fbbc27e26
SebastianKaltenbachIBM <Sebastian.Kaltenbach1@ibm.com>
2023-06-21 07:05:15 +0200

    Prepare reporter functionality

--- src/main/java/com/ibm/ReportingMojo.java
+++ src/main/java/com/ibm/ReportingMojo.java
@@ -8,7 +8,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import com.ibm.model.ModelCollector;
+import com.ibm.model.BpmnReporter;
 
 @Mojo(threadSafe = true, name = "report", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public class ReportingMojo extends AbstractMojo {
@@ -17,15 +17,25 @@ public class ReportingMojo extends AbstractMojo {
     MavenProject project;
 
     @Parameter(required = true, defaultValue = "${project.basedir}/src/main/resources", property = "name.source")
-    private String sourceFolder;
+    private String source;
 
     @Parameter(property = "resultFile", defaultValue = "target/generated-source/dummy/bpmn_validation.result.yaml")
     private String resultFile;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        ModelCollector collector = new ModelCollector(sourceFolder);
-        collector.collectModelsInSourceFolder();
-        getLog().info("Hello from Parser Mojo");
+        BpmnReporter reporter = new BpmnReporter(source).execute();
+
+        reporter.getModelCollection().forEach(e -> {
+            getLog().info("Analyzing model '" + e.getModel().getModelName() + "'");
+            getLog().info("");
+            getLog().info("Extraction of model objects");
+
+            getLog().info(reporter.prepareBpmnReport(e));
+
+            getLog().info("");
+        });
+
+        getLog().info("Hello from Reporter Mojo");
     }
 }
---
+++ src/main/java/com/ibm/model/BpmnReporter.java
@@ -0,0 +1,50 @@
+package com.ibm.model;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+
+import lombok.Getter;
+
+public class BpmnReporter {
+
+    private String source;
+
+    @Getter
+    private List<BpmnModelInstance> modelCollection;
+
+    private final FileFilter bpmnFilefilter = new FileFilter()
+    {
+        public boolean accept(File file) {
+            if (file.getName().endsWith(".bpmn")) {
+                return true;
+            }
+            return false;
+        }
+    };
+
+    public BpmnReporter(String source) {
+        this.source = source;
+    }
+
+    public BpmnReporter execute() {
+        File dir = new File(this.source);
+        modelCollection = Stream.of(dir.listFiles(bpmnFilefilter))
+            .map(e -> transformBpmnInstanceFromFile(e)).toList();
+
+        return this;
+    }
+
+    private BpmnModelInstance transformBpmnInstanceFromFile(File file) {
+        return Objects.requireNonNull(Bpmn.readModelFromFile(file));
+    }
+
+    public String prepareBpmnReport(BpmnModelInstance modelInstance) {
+        return modelInstance.getModel().getModelName();
+    }
+}
