db64ccfd4553da798a566d6ccd78e4d5fe0f7427
SebastianKaltenbachIBM <Sebastian.Kaltenbach1@ibm.com>
2023-07-29 16:24:11 +0200

    feat: Add rule

--- src/main/java/de/dsheng/linturtle/model/rules/common/complex/ParallelGatewayOutgoingFlowsNameNonNullOrEmpty.java
+++ src/main/java/de/dsheng/linturtle/model/rules/common/complex/ParallelGatewayOutgoingFlowsNameNonNullOrEmpty.java
@@ -14,17 +14,17 @@ import de.dsheng.linturtle.model.entity.Severity;
 import de.dsheng.linturtle.utils.ProcessUtils;
 import de.dsheng.linturtle.utils.RuleCheckUtils;
 
-@Rule(description = "Check if exclusive gateway flows have a name", severity = Severity.MUST, targetType = Element.PROCESS)
-public class ExclusiveGatewayFlowsNameNonNullOrEmpty extends ComplexRule {
+@Rule(description = "Check if parallel gateway flows have a name", severity = Severity.MUST, targetType = Element.PROCESS)
+public class ParallelGatewayOutgoingFlowsNameNonNullOrEmpty extends ComplexRule {
 
     @Override
     public Map<String, Boolean> check(TProcess process) {
         Map<String, Boolean> resultSet = new HashMap<>();
         List<String> flows = new ArrayList<>();
 
-        ProcessUtils.getAllOutgoingGateways(process, Element.EXCLUSIVEGATEWAY).stream()
-            .forEach(exclusiveGateway -> {
-                exclusiveGateway.getOutgoing().stream().forEach(flow -> {
+        ProcessUtils.getAllOutgoingGateways(process, Element.PARALLELGATEWAY).stream()
+            .forEach(parallelGateway -> {
+                parallelGateway.getOutgoing().stream().forEach(flow -> {
                     flows.add(flow.getLocalPart());
                 });
             });
